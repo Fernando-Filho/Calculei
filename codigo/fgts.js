800799935e825c011319cc1a2c86ca2e21c53120
Fernando Filho <fernando.fs.filhorn@gmail.com>
2022-07-25 09:26:28 -0300

    ajuste da função

--- codigo/fgts.js
+++ codigo/fgts.js
@@ -1,7 +1,8 @@
-const aliquotaFGTS = 8
-var valorFGTS
-
-export function fgts ( baseCalculo )
+function fgts ( baseCalculo )
 {
+    const aliquotaFGTS = 8
+    let valorFGTS
     valorFGTS = ((baseCalculo * aliquotaFGTS) / 100)
+    return valorFGTS
 }
+console.log(fgts(1500)
