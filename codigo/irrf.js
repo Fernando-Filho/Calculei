6cefec5d85f2a4359902a93ecfd2566ce0e6bd19
Fernando Filho <fernando.fs.filhorn@gmail.com>
2022-07-25 09:23:13 -0300

    tabela e condições ajustadas
    correção de bugs

--- codigo/irrf.js
+++ codigo/irrf.js
@@ -3,44 +3,40 @@ function irrf (baseCalculo, qtdDepentes)
     //Tabela de IRRF.
     const irrf =
     {
-        faixa1: [0, 1903.98, 0, 0],
-        faixa2: [1903.99, 2826.65, 7.5, 142.8],
-        faixa3: [2826.66, 3751.05, 15, 354.80],
-        faixa4: [3751.06, 4664.68, 22.5, 636.13],
-        faixa5: [4664.69, , 27.5, 869.36],
-        deduçãoPorDependente: 189.59
+        faixa1: [1903.98, 7.5, 142.8],
+        faixa2: [2826.65, 15, 354.80],
+        faixa3: [3751.05, 22.5, 636.13],
+        faixa4: [4664.68, 27.5, 869.36],
+        qtdDepentes: 189.59
     }
     
     // Constantes que serão necessárias para o calculo do IRRF.
-    var irrfDescontado
-    const valorInicial = 0
-    const valorFinal = 1
-    const alíquota = 2
-    const descontoMáximoAtéFaixa = 3
-    const parcelaDedutível = 3
+    let irrfDescontado
+    let deduçãoDosDependentes = qtdDepentes * qtdDepentes
+    const teto = 0
+    const alíquota = 1
+    const parcelaDedutível = 2
     //Calculo pra achar o valor a ser descontado no IRRF.
-    baseCalculo = baseCalculo - qtdDepentes
+    baseCalculo = baseCalculo - deduçãoDosDependentes
 
-    if( baseCalculo <= irrf.faixa1[valorFinal])
-    {
-        irrfDescontado = ((baseCalculo * irrf.faixa1[alíquota]) / 100) - irrf.faixa1[parcelaDedutível]
-    }
-    else if( baseCalculo <= irrf.faixa2[valorFinal])
-    {
-        irrfDescontado = ((baseCalculo * irrf.faixa2[alíquota]) / 100) - irrf.faixa2[parcelaDedutível]
-    }
-    else if( baseCalculo <= irrf.faixa3[valorFinal])
-    {
-        irrfDescontado = ((baseCalculo * irrf.faixa3[alíquota]) / 100) - irrf.faixa3[parcelaDedutível]
-    }
-    else if( baseCalculo <= irrf.faixa4[valorFinal])
+    if (baseCalculo >= irrf.faixa4[teto])
     {
         irrfDescontado = ((baseCalculo * irrf.faixa4[alíquota]) / 100) - irrf.faixa4[parcelaDedutível]
     }
-    else if( baseCalculo <= irrf.faixa5[valorFinal])
+    else if( baseCalculo >= irrf.faixa3[teto])
     {
-        irrfDescontado = ((baseCalculo * irrf.faixa5[alíquota]) / 100) - irrf.faixa5[parcelaDedutível]
+        irrfDescontado = ((baseCalculo * irrf.faixa3[alíquota]) / 100) - irrf.faixa3[parcelaDedutível]
     }
+    else if( baseCalculo >= irrf.faixa2[teto])
+    {
+        irrfDescontado = ((baseCalculo * irrf.faixa2[alíquota]) / 100) - irrf.faixa2[parcelaDedutível]
+    }
+    else if( baseCalculo >= irrf.faixa1[teto])
+    {
+        irrfDescontado = ((baseCalculo * irrf.faixa1[alíquota]) / 100) - irrf.faixa1[parcelaDedutível]
+    }
+    else { irrfDescontado = 0 }
+
     return irrfDescontado
 }
 console.log(irrf(2731, 2))
